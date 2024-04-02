78e4542370b29f91be2199dfa87ce5683305e264
Martim-Santos <128045103+Martim-Santos@users.noreply.github.com>
2024-04-02 11:16:10 +0100

    Adicionar arquivos de projeto.

---
+++ Lar de Idosos/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Lar_de_Idosos.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
