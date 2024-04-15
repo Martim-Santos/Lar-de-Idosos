7b2b78116a105f696aee75029ddefbd3488ae86b
Martim-Santos <128045103+Martim-Santos@users.noreply.github.com>
2024-04-15 18:11:09 +0100

    Model Enfermeiro - Inicio

---
+++ Lar de Idosos/Models/Enfermeiro.cs
@@ -0,0 +1,16 @@
+﻿namespace Lar_de_Idosos.Models {
+    public class Enfermeiro {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Idade { get; set; }
+
+        public string Email { get; set; }
+
+        public string NumTelemovel { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
