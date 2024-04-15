c2cb2f6d5cb5bbda3679709a6886d7cc34976771
Martim-Santos <128045103+Martim-Santos@users.noreply.github.com>
2024-04-15 17:58:37 +0100

    Model Gruardião - Inicio

---
+++ Lar de Idosos/Models/Guardiao.cs
@@ -0,0 +1,12 @@
+﻿namespace Lar_de_Idosos.Models {
+    public class Guardiao {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string NumTelemovel { get; set; }
+    }
+}
