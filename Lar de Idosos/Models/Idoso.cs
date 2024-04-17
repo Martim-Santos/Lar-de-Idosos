22ab2b0ce6058835de7915138dca220386e3a4b9
Martim-Santos <128045103+Martim-Santos@users.noreply.github.com>
2024-04-17 09:26:44 +0100

    Relacionamento entre Idoso e Trabalhador

--- Lar de Idosos/Models/Idoso.cs
+++ Lar de Idosos/Models/Idoso.cs
@@ -3,6 +3,10 @@
 namespace Lar_de_Idosos.Models {
     public class Idoso {
 
+        public Idoso() {
+            ListaTrabalhadores = new HashSet<Trabalhador>();
+        }
+
         public int Id { get; set; }
 
         public string Nome { get; set; }
@@ -20,5 +24,8 @@ namespace Lar_de_Idosos.Models {
         public int GuardiaoFK { get; set; } // FK para o Guardiao
         public Guardiao Guardiao { get; set; } // FK para o Guardiao
 
+
+        // relacionamento N-M, com atributos no relacionamento
+        public ICollection<Trabalhador> ListaTrabalhadores { get; set; }
     }
 }
