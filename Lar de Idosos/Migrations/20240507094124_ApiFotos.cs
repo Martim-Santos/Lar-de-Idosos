625f98ddb82a424f1b3a19a31b9ff46867b6ccc7
Martim-Santos <128045103+Martim-Santos@users.noreply.github.com>
2024-05-14 09:25:19 +0100

    Criação do Idoso na API - usar postman para testar criação do IdosoDTL que é usado para criar o idoso a partir da API

---
+++ Lar de Idosos/Migrations/20240507094124_ApiFotos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lar_de_Idosos.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApiFotos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "estado",
+                table: "Idoso",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "estado",
+                table: "Idoso");
+        }
+    }
+}
---
+++ Lar de Idosos/Models/IdosoDTL.cs
@@ -0,0 +1,12 @@
+﻿namespace Lar_de_Idosos.Models {
+    public class IdosoDTO {
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+        public int IdGuardiao { get; set; }
+
+        public IFormFile? Foto { get; set; }
+
+    }
+}
